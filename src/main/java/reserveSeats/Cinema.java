b78a335467a0e1edd092a72c3ea1f84173beba9a
DavidUyaguariJ <david.uyaguari@udla.edu.ec>
2024-08-29 22:31:19 -0500

    refactor variables from f (fila) to r(row)

--- src/main/java/reserveSeats/Cinema.java
+++ src/main/java/reserveSeats/Cinema.java
@@ -10,28 +10,28 @@ public class Cinema {
     }
     
     private void inizialiceData(){
-        for(int f=0;f<10; f++ ){
+        for(int r=0;r<10; r++ ){
            for(int c=0; c<10;c++){
-               sits[f][c]='L';
+               sits[r][c]='L';
            }
        }
     }
     public final void getSits(){
-        for(int f=0;f<10; f++ ){
+        for(int r=0;r<10; r++ ){
            for(int c=0; c<10;c++){
-               System.out.print(this.sits[f][c] + "   ");
+               System.out.print(this.sits[r][c] + "   ");
            }
             System.out.print('\n');
        }
     }
-    public final String reserveSits(int f, int c){
-        if((f>0&&c>0)&&(f<=10&&c<=10)){
-            if(this.sits[f-1][c-1]!='L'){
+    public final String reserveSits(int r, int c){
+        if((r>0&&c>0)&&(r<=10&&c<=10)){
+            if(this.sits[r-1][c-1]!='L'){
             return "El asiento ya esta ocupado no se puede reservar.";
             }
-            this.sits[f-1][c-1]='R';
+            this.sits[r-1][c-1]='X';
             this.getSits();
-            return "Se reservo Exitosamente en la fila: "+f +" y colunma: "+c;
+            return "Se reservo Exitosamente en la fila: "+r +" y colunma: "+c;
         }
         return "El numero que seleccionaste no es correcto, debe ser entre 1 y 10";
     }
--- src/main/java/reserveSeats/Reserve.java
+++ src/main/java/reserveSeats/Reserve.java
@@ -6,7 +6,7 @@ public class Reserve {
     
    public static void main (String [] args){
        Cinema cinema= new Cinema();
-       int menu, f,c;
+       int menu, r,c;
        System.out.println("Bienvenido");
        do{
            Scanner sc= new Scanner(System.in);
@@ -20,10 +20,10 @@ public class Reserve {
                cinema.getSits();
            }else if (menu==2){
                System.out.println("Ingresa la fila que deseas");
-               f=sc.nextInt();
+               r=sc.nextInt();
                System.out.println("Ingresa la columna que deseas");
                c=sc.nextInt();
-               System.out.println(cinema.reserveSits(f, c));
+               System.out.println(cinema.reserveSits(r, c));
             }else if(menu<0||menu>3){
                System.out.println("Opción Incorrecta, selecciona otra!");
            }
